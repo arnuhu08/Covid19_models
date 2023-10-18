9c58a3617ec89b77bba53fbe2a94e18d627a22b3
Arnuhu08 <arnuhu08@gmail.com>
2023-10-17 22:10:15 -0600

    add jupyter notebook file

--- model.py
+++ model.py
@@ -1,1 +1,7 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as numpy
+data = pd.read_csv("Covid Data.csv")
+df = data.drop(['INTUBED','ICU'], axis=1)
+print(df.head(10))
+# print(sum(df["PREGNANT"]==97))
